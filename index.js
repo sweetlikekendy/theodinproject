105a0dd85fde89eb0187ab65c7e631b1c7cec85a
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-09-07 15:00:32 -0700

    dist/main.js restaurant, index.js

--- index.js
+++ index.js
@@ -104,3 +104,54 @@ counter() // 3
 const a = 1
 let b = 2
 var c = 3
+
+const shapeInterface = (state) => {
+  return {
+    type: "shapeInterface",
+    area: () => state.area(state),
+  }
+}
+
+const solidShapeInterface = (state) => ({
+  type: "solidShapeInterface",
+  volume: () => state.volume(state),
+})
+
+const manageShapeInterface = (fn) => ({
+  type: "manageShapeInterface",
+  calculate: () => fn(),
+})
+
+const circle = (radius) => {
+  const proto = {
+    radius,
+    type: "Circle",
+    area: (args) => Math.PI * Math.pow(args.radius, 2),
+  }
+  const basics = shapeInterface(proto)
+  const abstraction = manageShapeInterface(() => basics.area())
+  const composite = Object.assign({}, basics, abstraction)
+  return Object.assign(Object.create(composite), { radius })
+}
+
+const cube = (length) => {
+  const proto = {
+    length,
+    type: "Cube",
+    area: (args) => Math.pow(args.length, 2),
+    volume: (args) => Math.pow(args.length, 3),
+  }
+  const basics = shapeInterface(proto)
+  const complex = solidShapeInterface(proto)
+  const abstraction = manageShapeInterface(
+    () => basics.area() + complex.volume()
+  )
+  const composite = Object.assign({}, basics, abstraction)
+  return Object.assign(Object.create(composite), { length })
+}
+
+const cir = circle(5)
+console.log(cir)
+
+const cubeObj = cube(5)
+console.log(cubeObj)
